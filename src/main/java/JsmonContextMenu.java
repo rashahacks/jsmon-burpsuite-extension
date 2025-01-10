6321cb67b52074b0121e0b3a3b8ce5bcf7753227
Vaibhav <singhvaibhav1710@gmail.com>
2025-01-10 22:51:12 +0530

    build and pushed

--- src/main/java/JsmonContextMenu.java
+++ src/main/java/JsmonContextMenu.java
@@ -54,36 +54,40 @@ public class JsmonContextMenu implements ContextMenuItemsProvider {
         SwingWorker<Void, String> worker = new SwingWorker<Void, String>() {
             @Override
             protected Void doInBackground() throws Exception {
-                String backendEndpoint = String.format("https://api.jsmon.sh/api/v2/uploadUrl?wkspId=%s", wkspId);
-                HttpClient client = HttpClient.newHttpClient();
+                String backendEndpoint =  String.format("https://api.jsmon.sh/api/v2/uploadUrl?wkspId=%s", wkspId);
+               try {
+                   HttpClient client = HttpClient.newHttpClient();
 
-                for (String url : uniqueUrls) {
-                    semaphore.acquire();
-                    try {
-                        String jsonPayload = String.format("{\"url\": \"%s\"}", url);
-                        long timme = System.currentTimeMillis()/1000;
-                        publish(jsonPayload+ " "+ timme +" \n");
+                   for (String url : uniqueUrls) {
+                       semaphore.acquire();
+                       try {
+                           String jsonPayload = String.format("{\"url\": \"%s\"}", url);
+                           long timme = System.currentTimeMillis() / 1000;
+                           publish(jsonPayload + " " + timme + " \n");
 
-                        HttpRequest request = HttpRequest.newBuilder()
-                                .uri(URI.create(backendEndpoint))
-                                .header("Content-Type", "application/json")
-                                .header("X-Jsmon-Key", apiKey)
-                                .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
-                                .build();
+                           HttpRequest request = HttpRequest.newBuilder()
+                                   .uri(URI.create(backendEndpoint))
+                                   .header("Content-Type", "application/json")
+                                   .header("X-Jsmon-Key", apiKey)
+                                   .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
+                                   .build();
 
-                        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                           HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-                        if (response.statusCode() == 200) {
-                            publish(url + " sent successfully!! Response: " + response.statusCode() + "\n");
-                        } else {
-                            publish("Failed to send " + url + "! Response code: " + response.statusCode() + "\n");
-                        }
-                    } catch (Exception e) {
-                        publish("Error processing " + url + " \n");
-                    }finally {
-                        semaphore.release();
-                    }
-                }
+                           if (response.statusCode() == 200) {
+                               publish(url + " sent successfully!! Response: " + response.statusCode() + "\n");
+                           } else {
+                               publish("Failed to send " + url + "! Response code: " + response.statusCode() + "\n");
+                           }
+                       } catch (Exception e) {
+                           publish("Error processing " + url + " \n");
+                       } finally {
+                           semaphore.release();
+                       }
+                   }
+               }catch(Exception e){
+                   publish("Exception Occured");
+               }
                 return null;
             }
 
